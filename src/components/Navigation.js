67446c46fb3faa8ba95202243851bfe048d63f60
your Carrot <stylenbs@gmail.com>
2021-02-02 18:42:28 +0900

    styles update

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTwitter } from "@fortawesome/free-brands-svg-icons";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faUser, faCarrot } from "@fortawesome/free-solid-svg-icons";
 
 const Navigation = ({ UserObject }) => {
   return (
     <>
       <nav>
-        <ul
-          style={{ display: "flex", justifyContent: "center", marginTop: 50 }}
-        >
+        <ul className="nav__container">
           <li>
             <Link to="/" style={{ marginRight: 10 }}>
-              <FontAwesomeIcon icon={faTwitter} color={"#04AAFF"} size="2x" />
+              <FontAwesomeIcon icon={faCarrot} color={"#04AAFF"} size="2x" />
             </Link>
           </li>
           <li>
