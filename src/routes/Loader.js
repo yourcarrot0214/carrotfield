d17b78a49d10a1edf82bf34c520043891b7e7574
your Carrot <stylenbs@gmail.com>
2021-02-04 18:51:18 +0900

    loading page styles update

--- src/routes/Loader.js
+++ src/routes/Loader.js
@@ -1,11 +1,11 @@
 import React from "react";
-import spinner from "./styles/spinner.gif";
+import { LoadingOutlined } from "@ant-design/icons";
 
 const Loader = () => {
   return (
     <>
       <div className="loader__container">
-        <img src={spinner} alt="Loading" className="loader__img" />
+        <LoadingOutlined className="loading__icon" />
       </div>
     </>
   );
